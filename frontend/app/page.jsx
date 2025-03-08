a079622c75a2d1b665b79dce258f2620121a2a08
MacBook Pro <macbookpro@MacBooks-MacBook-Pro.local>
2025-03-08 15:38:53 +0700

    page home

---
+++ frontend/app/page.jsx
@@ -0,0 +1,17 @@
+
+import SearchLocation from "@/components/SearchLocation";
+import SearchTour from "@/components/Search";
+import Banner from "@/components/BannerHotel"
+
+
+
+export default function Home() {
+  return (
+    <div >
+      
+      <SearchTour />
+      <Banner />
+
+    </div>
+  );
+}
